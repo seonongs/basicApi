bf4b04b893ab3ae6d1ec9f1ed061de711b8f74c9
seonongs <seonong@gmail.com>
2022-04-18 23:20:39 +0900

    changed

--- src/main/java/com/basicapi/user/UserController.java
+++ src/main/java/com/basicapi/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
